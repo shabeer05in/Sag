b46eadd2b58be08c0cd4b05bc5ee50e79b35e872
shabeer05in <info@sag4wd.com>
2025-10-07 17:06:34 +0400

    Add base SAG4WD theme

---
+++ theme/functions.php
@@ -0,0 +1,6 @@
+<?php
+function sag4wd_setup() {
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'sag4wd_setup');
+?>
---
+++ theme/index.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<h1 style="text-align:center; margin-top:100px;">🚀 SAG4WD Custom Theme Active</h1>
+<?php get_footer(); ?>
